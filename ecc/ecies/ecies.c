73c08d34cee6c87419a37119a8634f40612d2498
abbypan <abbypan@gmail.com>
2021-01-24 01:22:27 +0800

    fix bug strlen 00

--- ecc/ecies/ecies.c
+++ ecc/ecies/ecies.c
@@ -37,6 +37,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define BUFLEN 2048
+
 #endif
 
 void hexdump(unsigned char *info, unsigned char *buf, const int num)
@@ -83,15 +86,15 @@ char* get_random(int len){
     return buf;
 }
 
-int hkdf_hmac_sha256(unsigned char **out, char* key, char* salt, char* label, int L){
-    int key_len = sizeof(key);
+int hkdf_hmac_sha256(char* key, size_t key_len, char* salt, size_t salt_len, char* label, size_t label_len, unsigned char **out, size_t L){
+    /*int key_len = sizeof(key);*/
     hexdump("hkdf key", key, key_len);
 
-    int salt_len = strlen(salt);
+    /*int salt_len = sizeof(salt);*/
     hexdump("hkdf salt", salt, salt_len);
 
-    int label_len = strlen(label);
-    printf("\nhkdf label: %s\n", label);
+    /*int label_len = strlen(label);*/
+    /*printf("\nhkdf label: %s\n", label);*/
 
     printf("\nhkdf L: %d\n", L);
     *out = mbedtls_calloc(L, sizeof(unsigned char));
@@ -116,7 +119,6 @@ const mbedtls_pk_context * read_pk_from_cert(const char *path){
     /*printf("\n%s\n", buf);*/
     /*mbedtls_x509_crt_parse( crt, buf, n );*/
 
-
     /*const mbedtls_x509_time *time = &crt->valid_to;*/
     /*printf("\nvalid to: %d %d %d\n\n", time->year, time->mon, time->day);*/
 
@@ -132,7 +134,6 @@ const mbedtls_pk_context * read_pk_from_cert(const char *path){
 
 int sender_ecdh_z(const mbedtls_pk_context *receiver_pk, unsigned char **temp_pub, size_t *temp_pub_len, unsigned char **z, size_t *zlen ){
     size_t olen = 0; 
-    size_t buflen = 1024;
 
     mbedtls_ecdh_context ctx;
     mbedtls_ctr_drbg_context ctr_drbg;
@@ -149,18 +150,20 @@ int sender_ecdh_z(const mbedtls_pk_context *receiver_pk, unsigned char **temp_pu
     mbedtls_ecp_keypair *receiver_ecp = mbedtls_pk_ec( *receiver_pk );
     const mbedtls_ecp_group_id gid = receiver_ecp->grp.id;
     mbedtls_ecp_group_load( &ctx.grp, gid); 
+
+
     mbedtls_ecdh_gen_public( &ctx.grp,         
             &ctx.d,              // temp priv key
             &ctx.Q,              // temp public key
             mbedtls_ctr_drbg_random, 
             &ctr_drbg );            
 
-    int pbuf_len = mbedtls_mpi_size(&ctx.d);
+    size_t pbuf_len = mbedtls_mpi_size(&ctx.d);
     unsigned char *pbuf = mbedtls_calloc(pbuf_len, sizeof(unsigned char));
     mbedtls_mpi_write_binary( &ctx.d, pbuf, pbuf_len ); 
     hexdump("temp priv key",pbuf, pbuf_len);
 
-    /*unsigned char buf[1024];*/
+    /*unsigned char buf[BUFLEN];*/
     /*mbedtls_ecp_point_write_binary( &ctx.grp, &ctx.Q, MBEDTLS_ECP_PF_COMPRESSED, &olen, buf, buflen);*/
     /*temp_pub = mbedtls_calloc(olen, sizeof(unsigned char));*/
     /**temp_pub_len = olen;*/
@@ -170,7 +173,7 @@ int sender_ecdh_z(const mbedtls_pk_context *receiver_pk, unsigned char **temp_pu
 
     const mbedtls_pk_type_t pk_type = mbedtls_pk_get_type(receiver_pk);
     const mbedtls_pk_info_t *pk_info_t = mbedtls_pk_info_from_type( pk_type );
-    
+
     mbedtls_ecp_keypair *temp_ecp =  mbedtls_calloc( 1, sizeof( mbedtls_ecp_keypair ) );
     mbedtls_ecp_copy(&temp_ecp->Q, &ctx.Q); 
     mbedtls_mpi_copy(&temp_ecp->d, &ctx.d); 
@@ -179,8 +182,8 @@ int sender_ecdh_z(const mbedtls_pk_context *receiver_pk, unsigned char **temp_pu
     mbedtls_pk_context *temp_pk_ctx = mbedtls_calloc( 1, sizeof( mbedtls_pk_context ) );
     temp_pk_ctx->pk_info = pk_info_t;
     temp_pk_ctx->pk_ctx = temp_ecp;
-    unsigned char temp_pub_pem[2048];
-    mbedtls_pk_write_pubkey_pem(temp_pk_ctx, temp_pub_pem, 2048);
+    unsigned char temp_pub_pem[BUFLEN];
+    mbedtls_pk_write_pubkey_pem(temp_pk_ctx, temp_pub_pem, BUFLEN);
 
     *temp_pub_len = strlen(temp_pub_pem);
     *temp_pub = mbedtls_calloc(*temp_pub_len, sizeof(unsigned char));
@@ -191,10 +194,8 @@ int sender_ecdh_z(const mbedtls_pk_context *receiver_pk, unsigned char **temp_pu
     //set receiver public key
     mbedtls_ecp_copy(&ctx.Qp, &receiver_ecp->Q); //receiver public key
 
-
-    unsigned char tbuf[1024];
-    /*mbedtls_ecp_point_write_binary( &ctx.grp, &receiver_ecp->Q, MBEDTLS_ECP_PF_COMPRESSED, &olen, tbuf, buflen);*/
-    mbedtls_ecp_point_write_binary( &ctx.grp, &receiver_ecp->Q, MBEDTLS_ECP_PF_COMPRESSED, &olen, tbuf, buflen);
+    unsigned char tbuf[BUFLEN];
+    mbedtls_ecp_point_write_binary( &ctx.grp, &receiver_ecp->Q, MBEDTLS_ECP_PF_COMPRESSED, &olen, tbuf, BUFLEN);
     hexdump("receiver public key", tbuf, olen);
 
     //calc Z
@@ -231,7 +232,7 @@ static int write_file( const char *path, unsigned char *buf, size_t n )
     return( 0 );
 }
 
-int receiver_ecdh_z( mbedtls_pk_context *receiver_priv, unsigned char *temp_pub, size_t *temp_pub_len, unsigned char **z, size_t *zlen ){
+int receiver_ecdh_z( mbedtls_pk_context *receiver_priv, unsigned char *temp_pub, size_t temp_pub_len, unsigned char **z, size_t *zlen ){
 
     mbedtls_ctr_drbg_context ctr_drbg;
     mbedtls_entropy_context entropy;
@@ -250,28 +251,27 @@ int receiver_ecdh_z( mbedtls_pk_context *receiver_priv, unsigned char *temp_pub,
     mbedtls_ecp_group_load( &ctx.grp, gid); 
 
     mbedtls_mpi_copy(&ctx.d, &receiver_ecp->d);
-    int pbuf_len = mbedtls_mpi_size(&ctx.d);
+    size_t pbuf_len = mbedtls_mpi_size(&ctx.d);
     unsigned char *pbuf = mbedtls_calloc(pbuf_len, sizeof(unsigned char));
     mbedtls_mpi_write_binary( &ctx.d, pbuf, pbuf_len ); 
     hexdump("receiver priv key",pbuf, pbuf_len);
 
     mbedtls_pk_context *temp_pub_ctx = mbedtls_calloc( 1, sizeof( mbedtls_pk_context ) );
-    /*mbedtls_pk_context *temp_pub_ctx ;*/
-    /*hexdump("temp pub pem", temp_pub , *temp_pub_len);*/
-    printf("receive temp pub pem %d\n%s\n", *temp_pub_len, temp_pub);
 
-    write_file("temp_pub.pem", temp_pub, *temp_pub_len);
+    printf("receive temp pub pem %d\n%s\n", temp_pub_len, temp_pub);
+    write_file("temp_pub.pem", temp_pub, temp_pub_len);
 
-    /*int ret = mbedtls_pk_parse_public_key( temp_pub_ctx, temp_pub, *temp_pub_len);*/
-    int ret = mbedtls_pk_parse_public_keyfile(temp_pub_ctx, "temp_pub.pem");
+    /*int ret = mbedtls_pk_parse_public_keyfile(temp_pub_ctx, "temp_pub.pem");*/
+    int ret = mbedtls_pk_parse_public_key( temp_pub_ctx, temp_pub, temp_pub_len*8);
     printf("ret: %x\n", ret);
+
     mbedtls_ecp_keypair *temp_pub_ecp ;
     temp_pub_ecp = mbedtls_pk_ec( *temp_pub_ctx );
     mbedtls_ecp_copy(&ctx.Qp, &temp_pub_ecp->Q);
 
-    unsigned char buf[1024];
+    unsigned char buf[BUFLEN];
     size_t olen;
-    mbedtls_ecp_point_write_binary( &ctx.grp, &ctx.Qp, MBEDTLS_ECP_PF_COMPRESSED, &olen, buf, 1024);
+    mbedtls_ecp_point_write_binary( &ctx.grp, &ctx.Qp, MBEDTLS_ECP_PF_COMPRESSED, &olen, buf, BUFLEN);
     hexdump("temp public key", buf, olen);
 
     //calc Z
@@ -290,46 +290,47 @@ int receiver_ecdh_z( mbedtls_pk_context *receiver_priv, unsigned char *temp_pub,
     return 0;
 }
 
-int aes_gcm_encrypt(unsigned char* key, unsigned char* iv, unsigned char* input, unsigned char* aad, unsigned char** output, int* output_len, unsigned char** tag, size_t tag_len){
-    hexdump("aes encrypt: plaintext", input, strlen(input));
-    hexdump("aes key", key, strlen(key));
-    hexdump("aes iv", iv, strlen(iv));
-    hexdump("aes aad", aad, strlen(aad));
+int aes_gcm_encrypt(unsigned char* key, size_t key_len, unsigned char* iv, size_t iv_len, unsigned char* input, size_t input_len, unsigned char* aad, unsigned char **output, size_t* output_len, unsigned char** tag, size_t tag_len){
+    hexdump("aes encrypt: plaintext", input, input_len);
+    hexdump("aes encrypt key", key, key_len);
+    hexdump("aes encrypt iv", iv, iv_len);
+    hexdump("aes encrypt aad", aad, strlen(aad));
 
-    *output_len = strlen(input);
+    *output_len = input_len;
+    *output = mbedtls_calloc(*output_len, sizeof(unsigned char));
 
     mbedtls_gcm_context aes;
     mbedtls_gcm_init( &aes );
-    mbedtls_gcm_setkey( &aes,MBEDTLS_CIPHER_ID_AES , (const unsigned char*) key, strlen(key)*8);
-    mbedtls_gcm_starts(&aes, MBEDTLS_GCM_ENCRYPT, (const unsigned char*)iv, strlen(iv), (const unsigned char*) aad, strlen(aad));
-    mbedtls_gcm_update(&aes,strlen(input),(const unsigned char*)input, *output);
+    mbedtls_gcm_setkey( &aes,MBEDTLS_CIPHER_ID_AES , (const unsigned char*) key, key_len*8);
+    mbedtls_gcm_starts(&aes, MBEDTLS_GCM_ENCRYPT, (const unsigned char*)iv, iv_len, (const unsigned char*) aad, strlen(aad));
+    mbedtls_gcm_update(&aes,input_len,(const unsigned char*)input, *output);
     mbedtls_gcm_finish(&aes, *tag, tag_len);
     mbedtls_gcm_free( &aes );
 
-    hexdump("aes encrypt: tag", *tag, strlen(*tag));
-    hexdump("aes encrypt: cipher", *output, strlen(*output));
+    hexdump("aes encrypt: tag", *tag, tag_len);
+    hexdump("aes encrypt: cipher", *output, *output_len);
 
     return 0;
 }
 
-int aes_gcm_decrypt(unsigned char* key, unsigned char* iv, unsigned char* input, unsigned char* aad, unsigned char** output, int* output_len, unsigned char** tag, size_t tag_len){
-    hexdump("aes decrypt: cipher", input, strlen(input));
-    hexdump("aes key", key, strlen(key));
-    hexdump("aes iv", iv, strlen(iv));
-    hexdump("aes aad", aad, strlen(aad));
+int aes_gcm_decrypt(unsigned char* key, size_t key_len, unsigned char* iv, size_t iv_len, unsigned char* input, size_t input_len, unsigned char* aad, unsigned char** output, size_t* output_len, unsigned char* tag, size_t tag_len){
+    hexdump("aes decrypt: cipher", input, input_len);
+    hexdump("aes decrypt key", key, key_len);
+    hexdump("aes decrypt iv", iv, iv_len);
+    hexdump("aes decrypt aad", aad, strlen(aad));
+    hexdump("aes decrypt: tag", tag, tag_len);
 
-    *output_len = strlen(input);
+    *output_len = input_len;
 
     mbedtls_gcm_context aes;
     mbedtls_gcm_init( &aes );
-    mbedtls_gcm_setkey( &aes,MBEDTLS_CIPHER_ID_AES , (const unsigned char*) key, strlen(key) * 8);
-    mbedtls_gcm_starts(&aes, MBEDTLS_GCM_DECRYPT, (const unsigned char*)iv, strlen(iv), (const unsigned char*) aad, strlen(aad));
-    mbedtls_gcm_update(&aes,strlen(input),(const unsigned char*)input, *output);
-    mbedtls_gcm_finish(&aes, *tag, tag_len);
+    mbedtls_gcm_setkey( &aes,MBEDTLS_CIPHER_ID_AES , (const unsigned char*) key, key_len * 8);
+    mbedtls_gcm_starts(&aes, MBEDTLS_GCM_DECRYPT, (const unsigned char*)iv, iv_len, (const unsigned char*) aad, strlen(aad));
+    mbedtls_gcm_update(&aes,input_len,(const unsigned char*)input, *output);
+    mbedtls_gcm_finish(&aes, tag, tag_len);
     mbedtls_gcm_free( &aes );
 
-    hexdump("aes decrypt: plaintext", *output, strlen(*output));
-    /*hexdump("aes encrypt: tag", *tag, strlen(*tag));*/
+    hexdump("aes decrypt: plaintext", *output, *output_len);
 
     return 0;
 }
@@ -339,38 +340,36 @@ void main( int argc, char *argv[] )
 {
     //sender
     printf("sender:\n\n");
-    char cert_f[1024] = "receiver_ee_cert.pem";
+    char cert_f[BUFLEN] = "receiver_ee_cert.pem";
 
     const mbedtls_pk_context *pk = read_pk_from_cert(cert_f);
-    size_t temp_pub_len;
     unsigned char *temp_pub;
+    size_t temp_pub_len;
     unsigned char *sender_z;
     size_t sender_zlen;
     sender_ecdh_z(pk, &temp_pub, &temp_pub_len, &sender_z, &sender_zlen);
 
 
-    int salt_len = 32;
-    unsigned char *salt = get_random(32);
+    size_t salt_len = 32;
+    unsigned char *salt = get_random(salt_len);
 
-    unsigned char label[1024] = "someapp.somebusiness.somelabel";
+    unsigned char label[BUFLEN] = "someapp.somebusiness.somelabel";
 
-    int L = 32;
+    size_t L = 32;
     unsigned char *okm; 
-    hkdf_hmac_sha256(&okm, sender_z, salt, label, L);
-
+    hkdf_hmac_sha256(sender_z, sender_zlen, salt, salt_len, label, strlen(label), &okm, L);
 
     //encrypt data
-    int iv_len = 12;
+    size_t iv_len = 12;
     unsigned 	char *iv = get_random(iv_len);
     unsigned 	char data[] = "fujian quanzhou 66666666666666666666666666666666666";
     unsigned     char aad[] = "somedevice.context";
+
+    size_t cipher_info_len;
+    unsigned char *cipher_info;
     size_t tag_len = 16;
     unsigned     char *tag = mbedtls_calloc(tag_len, sizeof(unsigned char));
-
-    unsigned char *cipher_data = mbedtls_calloc(strlen(data), sizeof(unsigned char));
-    unsigned char **cipher_info = &cipher_data;
-    int cipher_info_len;
-    aes_gcm_encrypt(okm, iv, data, aad, cipher_info, &cipher_info_len, &tag, tag_len);
+    aes_gcm_encrypt(okm, L, iv, iv_len, data, strlen(data), aad, &cipher_info, &cipher_info_len, &tag, tag_len);
 
     //receiver
     printf("\nreceiver:\n\n");
@@ -380,12 +379,11 @@ void main( int argc, char *argv[] )
 
     unsigned char *receiver_z;
     size_t receiver_zlen;
-    receiver_ecdh_z( receiver_priv, temp_pub, &temp_pub_len, &receiver_z, &receiver_zlen);
+    receiver_ecdh_z( receiver_priv, temp_pub, temp_pub_len, &receiver_z, &receiver_zlen);
     unsigned char *r_okm;
-    printf("receiver_z len: %d\n", strlen(receiver_z));
-    hkdf_hmac_sha256(&r_okm, receiver_z, salt, label, L);
+    hkdf_hmac_sha256(receiver_z, receiver_zlen, salt, salt_len, label, strlen(label), &r_okm,  L);
 
-    unsigned char *plaintext =  mbedtls_calloc(strlen(*cipher_info), sizeof(unsigned char));
-    int plaintext_len;
-    aes_gcm_decrypt(r_okm, iv, *cipher_info, aad, &plaintext, &plaintext_len, &tag, tag_len);
+    unsigned char *plaintext =  mbedtls_calloc(cipher_info_len, sizeof(unsigned char));
+    size_t plaintext_len;
+    aes_gcm_decrypt(r_okm, L, iv, iv_len, cipher_info, cipher_info_len, aad, &plaintext, &plaintext_len, tag, tag_len);
 }
