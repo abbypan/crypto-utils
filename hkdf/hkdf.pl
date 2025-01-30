40c0f7984e743d134fcf9a7d0bba6bb6b746e112
abbypan <abbypan@gmail.com>
2025-01-31 04:02:08 +0800

    hkdf

--- hkdf/hkdf.pl
+++ hkdf/hkdf.pl
@@ -2,15 +2,12 @@
 
 use Crypt::KeyDerivation ':all';
 
-#$ perl hkdf.pl a27e195cf3ea9755eceb1f77ca0dd20ba1fdaa8832f1b2fb637c8912ad3dce13 dc4dab0be272e8e85afb0aa1d423813bf9a5a2c31d14dd231992aabb4f6fc6f0 somelabel 32 SHA256
-#f1bf30afd3f7c964a750244ff2e1daed8ad130fe12ff2cb844bd9d556c10e39e
+my ($password_hexstr, $salt_hexstr, $info, $byte_len, $hash_name) = @ARGV;
 
-my ($password, $salt, $info, $byte_len, $hash_name) = @ARGV;
-#hash name:SHA256 
+my $password = pack("H*", $password_hexstr);
+my $salt = pack("H*",$salt_hexstr);
 
-$password = pack("H*", $password);
-$salt = pack("H*",$salt);
+my $okm = hkdf($password, $salt, $hash_name, $byte_len, $info);
 
-$okm2 = hkdf($password, $salt, $hash_name, $byte_len, $info);
-
-printf("%s\n", unpack("H*", $okm2));
+my $okm_hexstr = unpack("H*", $okm);
+printf("%s\n", $okm_hexstr);
